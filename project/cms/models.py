a3c8085a0282f5ee0a94631a5a7164d5e1f8c049
c.borao.2017 <c.borao.2017@alumnos.urjc.es>
2021-04-21 21:15:14 +0200

    cms basico v1

--- project/cms/models.py
+++ project/cms/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Content(models.Model):
+
+    key = models.CharField(max_length=64)
+    value = models.TextField()
--- project/cms/views.py
+++ project/cms/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from .models import Content
+from django.views.decorators.csrf import csrf_exempt
+
+formulario = """
+No existe valor en la base de datos para esta llave.
+<p>Introdúcela:
+<p>
+<form action="" method="POST">
+    Valor: <input type="text" name="valor">
+    <br/><input type="submit" value="Submit">
+</form>
+"""
+
+
+@csrf_exempt
+def get_content(request, key):
+
+    if request.method == "POST":
+        value = request.POST['valor']
+
+        try:
+            # if the shortened url have been stored in a previous POST, we
+            # remove it and add the new content.
+            content = Content.objects.get(key=key)
+            content.delete()
+        except Content.DoesNotExist:
+            pass
+
+        content = Content(key=key, value=value)
+        content.save()
+        content.save()
+
+    elif request.method == "PUT":
+        value = request.body.decode('utf-8')
+        try:
+            content = Content.objects.get(key=key)
+            content.delete()
+        except Content.DoesNotExist:
+            pass
+
+        content = Content(key=key, value=value)
+        content.save()
+
+    try:
+        response = Content.objects.get(key=key).value
+    except Content.DoesNotExist:
+        response = 'No existe contenido para la clave ' + value + '\n' + formulario
+    except Content.MultipleObjectsReturned:
+        respuesta = Content.objects.filter(key=key).last().valor
+
+    return HttpResponse(response)
