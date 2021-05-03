e4e6f2a70a16aa9726e0792f713eeb8c06f97bbd
c.borao.2017 <c.borao.2017@alumnos.urjc.es>
2021-05-03 16:40:44 +0200

    cambiado orden urls.py

--- project/project/urls.py
+++ project/project/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.contrib.auth.views import LoginView as login
 
 urlpatterns = [
+    path('login', login.as_view()),
     path('cms/', include('cms.urls')),
     path('admin/', admin.site.urls),
-    path('login', login.as_view()),
 ]
